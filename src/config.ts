e04b050838bff42d4d530df5e5d383b750e4735e
Johan <johan@macair2020>
2022-05-03 15:43:12 +0200

    close #2

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,14 @@
-export const version = import.meta.env.VERSION || "unset_version";
+export const version = import.meta.env.VITE_VERSION || "unset_version";
 export const sha = import.meta.env.GIT_SHA || "unset_sha";
 export const basename = import.meta.env.PUBLIC_URL;
 
 export const title = "Vite Boilerplate";
 
+const { env } = import.meta;
+
 const ghUrl = "https://github.com/nexys-system/vite_boilerplate";
+
+console.log({ env });
 
 export const github = {
   sha: `${ghUrl}/commit/${sha}`,
