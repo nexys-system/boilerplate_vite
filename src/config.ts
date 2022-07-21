b7111b1dca64e86b046d3263a1c6954797c41241
John B <johan@nexys.ch>
2022-07-21 09:08:35 +0200

    fix env var reference and release url

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,15 @@
 export const title = "Vite Boilerplate";
 
 export const version: string = import.meta.env.VITE_VERSION || "unset_version";
-export const sha: string = import.meta.env.GIT_SHA || "unset_sha";
+export const sha: string = import.meta.env.VITE_GIT_SHA || "unset_sha";
 export const basename = import.meta.env.BASE_URL;
 
 const { env } = import.meta;
-
-console.log({ env });
 
 const ghUrl = "https://github.com/nexys-system/boilerplate_vite";
 
 export const github = {
   sha: `${ghUrl}/commit/${sha}`,
-  version: `${ghUrl}/release/tag/${version}`,
+  version: `${ghUrl}/releases/tag/${version}`,
   url: ghUrl,
 };
