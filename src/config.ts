d0cbc4736bb8fe975f17a10191b4240292afd2ea
John B <johan@nexys.ch>
2022-08-08 10:07:40 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,9 @@
 export const title = "Vite Boilerplate";
+const ghUrl = "https://github.com/nexys-system/boilerplate_vite";
 
 export const version: string = import.meta.env.VITE_VERSION || "unset_version";
 export const sha: string = import.meta.env.VITE_GIT_SHA || "unset_sha";
 export const basename = import.meta.env.BASE_URL;
-
-const { env } = import.meta;
-
-const ghUrl = "https://github.com/nexys-system/boilerplate_vite";
 
 export const github = {
   sha: `${ghUrl}/commit/${sha}`,
