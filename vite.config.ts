f8795bee1b18cc0789c5becbbc36b74e6baada5f
John B <johan@nexys.ch>
2022-08-09 11:32:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
+// this the default/base configuration
+const baseConfig =  {
+  plugins: [react()],
+  test: {
+    // ...
+  }
+};
+
 // https://vitejs.dev/config/
 // conditional config: https://vitejs.dev/config/#conditional-config
 export default defineConfig(({ command, mode, ssrBuild }) => {
-  console.log({ command, mode, ssrBuild });
-  const baseConfig =  {
-    plugins: [react()],
-    test: {
-      // ...
-    }
-  };
+  // console.log({ command, mode, ssrBuild });
   
+  // if  `mode===ghpages`: we add the path prefix to match the path prefix of gh pages
   if (mode === 'ghpages') {
     return {...baseConfig, base: "/boilerplate_vite/" };
   }
