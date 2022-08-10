f3323752aa3d0c9e74e657bfe862e169818874ac
John B <johan@nexys.ch>
2022-08-10 07:46:49 +0200

    Create links.ts

--- src/links.ts
+++ src/links.ts
@@ -1,4 +1,9 @@
-export const links = {home: {link: '/', name: 'Home'}};
+interface Link {
+  name: string;
+  link: string;
+}
+
+export const links: { [k: string]: Link } = { = {home: {link: '/', name: 'Home'}};
 
 export const menus: { name: string; link: string }[] = Object.values(links);
 
