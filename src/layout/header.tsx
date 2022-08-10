2ff7617f2d9fed59ad98f3d329b9f9a69f47f47d
John B <johan@nexys.ch>
2022-08-10 07:37:38 +0200

    upgrade to bootstrap5

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -13,7 +13,7 @@ export default () => (
       style={style}
       className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white"
     >
-      <h5 className="my-0 mr-md-auto font-weight-normal">
+      <h5 className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <Link to={"/"}>{title}</Link>
       </h5>
       <nav className="my-2 my-md-0 mr-md-3">
