9f8c67668824b4423fe6a5b111727c23413ce69b
John B <johan@nexys.ch>
2022-08-10 07:47:03 +0200

    Update links.ts

--- src/links.ts
+++ src/links.ts
@@ -3,7 +3,7 @@ interface Link {
   link: string;
 }
 
-export const links: { [k: string]: Link } = { = {home: {link: '/', name: 'Home'}};
+export const links: { [k: string]: Link } = {home: {link: '/', name: 'Home'}};
 
 export const menus: { name: string; link: string }[] = Object.values(links);
 
