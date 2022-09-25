de300be7f06737edcdffbc986e568f63441bf730
John B <johan@nexys.ch>
2022-09-25 12:03:39 +0200

    Update index.tsx

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,22 +1,17 @@
 import Footer from "./footer";
 import Header from "./header";
 
-import { BrowserRouter as Router } from "react-router-dom";
-import { basename } from "../config";
-
 function Layout({ children }: { children: any }) {
   return (
-    <Router basename={basename}>
-      <div className="d-flex flex-column min-vh-100">
-        <Header />
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
 
-        <main className="flex-fill">
-          <div className="container">{children} </div>
-        </main>
+      <main className="flex-fill">
+        <div className="container">{children} </div>
+      </main>
 
-        <Footer />
-      </div>
-    </Router>
+      <Footer />
+    </div>
   );
 }
 export default Layout;
