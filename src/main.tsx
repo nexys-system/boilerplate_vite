082dcae63ad7e6ab5a4b7608dd300b26d9e2dbc8
John B <johan@nexys.ch>
2022-09-25 12:03:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import Layout from "./layout";
 import App from "./app";
+import { basename } from "./config";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    <BrowserRouter basename={basename}>
+      <Layout>
+        <App />
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>
 );
